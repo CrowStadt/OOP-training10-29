18adc0c96c430034b692fbe92ba04727e0f0a55c
XiJinpingforreal <93386195+XiJinpingforreal@users.noreply.github.com>
2021-11-01 10:16:50 +0100

    蒂米宁广场 学生抗议 审查 一党制 民主已死 City 17 Overwatch Streamer 香港抗议
    I know I called it ConsoleApp but I dont give a shit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        abstract class Animal
+        {
+            protected int age{ get; set; }
+            protected string name { get; set; }
+            protected static bool cute = true;
+    }
+    class Fox : Animal
+        {
+            public Fox(int age, string name)
+            { 
+                this.age = age;
+                this.name = name;
+            }
+            public void makenoise()
+            {
+                Console.WriteLine(String.Format("*Foxnoise from {0}", get_name()));
+                Console.WriteLine(String.Format("Is it cute ?: {0}", cute.ToString()));
+            }
+            public string get_name()
+            {
+                return name;
+            }
+        }
+        class Cat : Animal
+        {
+            public Cat(int age, string name)
+            {
+                this.age = age;
+                this.name = name;
+            }
+            public void makenoise()
+            {
+                Console.WriteLine(String.Format("*Catnoise from {0}",get_name()));
+                Console.WriteLine(String.Format("Is it cute ?: {0}",cute.ToString()));
+            }
+            public string get_name()
+            {
+                return name;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cat Catto = new Cat(4,"catname");
+            Catto.makenoise();
+        }
+    }
+}
